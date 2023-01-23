e656ad2573737377191e84898974211ade16c68f
Ian <ianmayw@gmail.com>
2023-01-23 11:32:08 -0500

    Optimize and update main activity logic

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -7,39 +7,31 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.calculateButton.setOnClickListener{ calculateTip() }
+        binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
-    fun calculateTip() {
+    private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
 
-        val selectedID = binding.tipOptions.checkedRadioButtonId
         val tipPercentage =
-            when (selectedID) {
+            when (binding.tipOptions.checkedRadioButtonId) {
                 R.id.option_twenty_percent -> 0.20
                 R.id.option_eighteen_percent -> 0.18
                 else -> 0.15
-        }
-
+            }
         var tip = tipPercentage * cost
-
         val roundUp = binding.roundUpSwitch.isChecked
-
         if (roundUp) {
             tip = kotlin.math.ceil(tip)
         }
-
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-
-
     }
 }
